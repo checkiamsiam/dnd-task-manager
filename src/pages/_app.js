e6c22dba341aaf9cf23615e0a600ce767fcbe671
checkiamsiam <issiam02415@gmail.com>
2023-08-18 16:57:45 +0600

    startup

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const getLayout = Component.getLayout || ((page) => <>{page}</>);
+  return getLayout(
+    <>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
+  );
 }
